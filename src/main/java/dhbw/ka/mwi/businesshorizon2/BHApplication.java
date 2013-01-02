24f1d81252e41d963e210a8aad6376c8a0495304
Julius Hacker <info@julius-hacker.de>
2013-01-02 12:10:28 +0100

    Integration initalScreen und Prozess-GUI

--- src/main/java/dhbw/ka/mwi/businesshorizon2/BHApplication.java
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/BHApplication.java
@@ -1,14 +1,18 @@
 package dhbw.ka.mwi.businesshorizon2;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.mvplite.event.EventBus;
+import com.mvplite.event.EventHandler;
 import com.vaadin.Application;
 
 import dhbw.ka.mwi.businesshorizon2.ui.initialscreen.InitialScreenViewImpl;
 import dhbw.ka.mwi.businesshorizon2.ui.initialscreen.ShowInitialScreenViewEvent;
 import dhbw.ka.mwi.businesshorizon2.ui.main.MainViewImpl;
+import dhbw.ka.mwi.businesshorizon2.ui.main.ShowMainViewEvent;
+import dhbw.ka.mwi.businesshorizon2.ui.projectlist.ShowProjectEvent;
 
 /**
  * Das ist die Haupt-Einstiegsklasse der Anwendung. Es ist vergleichbar mit der
@@ -20,9 +24,11 @@ import dhbw.ka.mwi.businesshorizon2.ui.main.MainViewImpl;
  */
 public class BHApplication extends Application {
 	private static final long serialVersionUID = 1L;
+	
+	private Logger logger = Logger.getLogger("BHApplication.class");
 
 	@Autowired
-	private MainViewImpl mainView;
+	private MainViewImpl processView;
 	
 	@Autowired
 	private InitialScreenViewImpl initialScreenView;
@@ -43,17 +49,33 @@ public class BHApplication extends Application {
 	
 	/**
 	 * Diese Methode ist dafuer verantwortlich, das Haupt-Fenster zu laden und
-	 * den ShowMainViewEvent abzusetzen. Durch diesen werden die Listener (z.B. der MainPresenter)
+	 * den ShowInitialScreenViewEvent abzusetzen. Durch diesen werden die Listener
 	 * darueber informiert, dass das Hauptfenster angezeigt wird.
 	 * 
 	 * @author Christian Gahlert
 	 */
 	@Override
 	public void init() {
-
+		eventBus.addHandler(this);
+		
 		setMainWindow(initialScreenView);
 		eventBus.fireEvent(new ShowInitialScreenViewEvent());
 		
+	}
+
+	/**
+	 * Die Methode triggert die Anzeige der Prozessansicht, sobald an einer Stelle
+	 * des Programmes ein Projekt angezeigt wurde.
+	 * 
+	 * @author Julius Hacker
+	 * @param event Der ausgeloeste ShowProjectEvent
+	 */
+	@EventHandler
+	public void showProcessView(ShowProjectEvent event) {
+		setMainWindow(processView);
+		this.removeWindow(initialScreenView);
+		eventBus.fireEvent(new ShowMainViewEvent());
+		logger.debug("ShowMainViewEvent gefeuert");
 	}
 	
 
