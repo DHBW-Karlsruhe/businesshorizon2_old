4b07b654e792df7cf391f941b5ac5b1123377ed3
Florian Stier <Florian Stier@172.16.14.17>
2013-01-04 12:54:33 +0100

    - BusinessHorizionException Klasse angepasst nacht Merge von MASTER branch

--- src/main/java/dhbw/ka/mwi/businesshorizon2/BusinessHorizonException.java
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/BusinessHorizonException.java
@@ -1,19 +1,17 @@
 package dhbw.ka.mwi.businesshorizon2;
 
 /**
- * Exception Klasse, von der alle anderen selbst angelegten Exceptionklassen
- * erben.
+ * Diese Klasse stellt die zentrale Exception Klasse der Anwendung dar. Alle
+ * selbst erstellten Exceptions sollen von dieser Klasse erben. Den Messages
+ * wird jeweils 'Business Horizon Application Error' voran gestellt.
  * 
  * @author Florian Stier
  * 
  */
-
 public class BusinessHorizonException extends Exception {
 
-	private static final long serialVersionUID = 1L;
-
 	public BusinessHorizonException(String s) {
-		super("Business Horizon Applicaton Error: " + s);
+		super("Business Horizon Application Error: " + s);
 	}
 
 }
