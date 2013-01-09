5d04541c503ceeaf9fd529bc5648f45dae28e4e0
daniel dengler <dani.dengler@googlemail.com>
2013-01-09 11:23:35 +0100

    Authentification gefixed

---
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/services/authentication/AuthenticationServiceInterface.java
@@ -0,0 +1,63 @@
+package dhbw.ka.mwi.businesshorizon2.services.authentication;
+
+import dhbw.ka.mwi.businesshorizon2.models.User;
+
+
+
+/**
+ * Dieses Interface stellt Methoden zur Authentifizierung einzelner Benutzer zur
+ * Verfügung. Durch Bereitstellung des Interfaces ist es möglich die
+ * implementierende(n) Klasse(n) auszutauschen und die Kommunikation zwischen
+ * Applikation und Nutzerdatenbank anzupassen.
+ * 
+ * @author Florian Stier
+ * 
+ */
+public interface AuthenticationServiceInterface {
+
+	/**
+	 * Methode zum einloggen eines Benutzers. Implementierende Klassen müssen
+	 * die Kommunikation mit der User Datenbank herstellen und die
+	 * Authentifizierung durchführen.
+	 * 
+	 * @param username
+	 *            Anmeldename eines Nutzers
+	 * @param password
+	 *            Passwort des Nutzers
+	 * @return User Objekt mit Benutzerdaten
+	 */
+	public User doLogin(String username, String password);
+
+	/**
+	 * Methode zum Ausloggen eines Users.
+	 * 
+	 * @param user
+	 *            User der ausgeloggt werden soll
+	 */
+	public void doLogout(User user);
+
+	/**
+	 * Methode zum registrieren eines neuen Nutzers. Implementierende Klassen
+	 * müssen eine Verbindung zur Datenbank herstellen und einen neuen User mit
+	 * den übergebenen Parametern anlegen.
+	 * 
+	 * @param username
+	 *            Name zur Anmeldung eines Users
+	 * @param password
+	 *            Passwort zum Anmeldenamen
+	 * @param firstName
+	 *            Vorname des Anwenders
+	 * @param lastName
+	 *            Nachname des Anwenders
+	 */
+	public void registerNewUser(String username, String password,
+			String firstName, String lastName);
+
+	/**
+	 * Methode zum Löschen des als Parameter übergebenen Users.
+	 * 
+	 * @param user
+	 *            der zu löschende User
+	 */
+	public void deleteUser(User user);
+}
