2c52904c2d2cb28789c25fdc82b3bb0dc81384d3
Timo Belz <timo_belz@web.de>
2013-01-12 16:08:20 +0100

    Sicherung vor dem Versuch den Master zu mergen

---
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/ui/process/method/InputTypeChangedEvent.java
@@ -0,0 +1,12 @@
+package dhbw.ka.mwi.businesshorizon2.ui.process.method;
+
+import com.mvplite.event.Event;
+
+public class InputTypeChangedEvent extends Event {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
