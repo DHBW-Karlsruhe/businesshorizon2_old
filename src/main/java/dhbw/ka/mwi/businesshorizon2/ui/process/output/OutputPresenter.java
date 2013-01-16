de76b19937bfe0917a79558e99ee537ade62c10f
Florian Stier <Florian Stier@172.16.19.123>
2013-01-16 17:59:33 +0100

    - neue Testwerte und erster Aufruf der Zeitreihenanalyse

--- src/main/java/dhbw/ka/mwi/businesshorizon2/ui/process/output/OutputPresenter.java
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/ui/process/output/OutputPresenter.java
@@ -1,5 +1,7 @@
 package dhbw.ka.mwi.businesshorizon2.ui.process.output;
 
+import java.util.TreeSet;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.log4j.Logger;
@@ -8,6 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.mvplite.event.EventBus;
 import com.mvplite.event.EventHandler;
 
+import dhbw.ka.mwi.businesshorizon2.methods.CallbackInterface;
+import dhbw.ka.mwi.businesshorizon2.methods.StochasticMethodException;
+import dhbw.ka.mwi.businesshorizon2.methods.timeseries.TimeseriesCalculator;
+import dhbw.ka.mwi.businesshorizon2.models.CashFlowPeriod;
+import dhbw.ka.mwi.businesshorizon2.models.Project;
+import dhbw.ka.mwi.businesshorizon2.models.StochasticResultContainer;
 import dhbw.ka.mwi.businesshorizon2.ui.process.ScreenPresenter;
 import dhbw.ka.mwi.businesshorizon2.ui.process.ScreenSelectableEvent;
 import dhbw.ka.mwi.businesshorizon2.ui.process.ShowErrorsOnScreenEvent;
@@ -23,13 +31,16 @@ import dhbw.ka.mwi.businesshorizon2.ui.process.output.charts.StochasticChartArea
  * 
  */
 
-public class OutputPresenter extends ScreenPresenter<OutputViewInterface> {
+public class OutputPresenter extends ScreenPresenter<OutputViewInterface> implements CallbackInterface {
 	private static final long serialVersionUID = 1L;
 
 	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Autowired
 	private EventBus eventBus;
+
+	@Autowired
+	private TimeseriesCalculator timeSeriesCalculator;
 
 	/**
 	 * Dies ist der Konstruktor, der von Spring nach der Initialierung der
@@ -48,12 +59,46 @@ public class OutputPresenter extends ScreenPresenter<OutputViewInterface> {
 
 		// ausgewählte Berechnungsverfahren holen
 		// Für jedes Berechnungsverfahren die Berechnung triggern
+
+		StochasticResultContainer src;
+		TreeSet<CashFlowPeriod> periods = new TreeSet<CashFlowPeriod>();
+		CashFlowPeriod period1 = new CashFlowPeriod(2005);
+		period1.setFreeCashFlow(130594000.00);
+		periods.add(period1);
+		CashFlowPeriod period2 = new CashFlowPeriod(2006);
+		period2.setFreeCashFlow(147552000.00);
+		periods.add(period2);
+		CashFlowPeriod period3 = new CashFlowPeriod(2007);
+		period3.setFreeCashFlow(144040000.00);
+		periods.add(period3);
+		CashFlowPeriod period4 = new CashFlowPeriod(2008);
+		period4.setFreeCashFlow(146004000.00);
+		periods.add(period4);
+		CashFlowPeriod period5 = new CashFlowPeriod(2009);
+		period5.setFreeCashFlow(154857000.00);
+		periods.add(period5);
+		CashFlowPeriod period6 = new CashFlowPeriod(2010);
+		period6.setFreeCashFlow(162117000.00);
+		periods.add(period6);
+		Project project = new Project("Test Zeitreihenanalyse");
+		project.setBasisYear(2010);
+		project.setIterations(10000);
+		project.setRelevantPastPeriods(2);
+		project.setPeriodsToForecast(5);
+		project.setPeriods(periods);
+
+		try {
+			src = timeSeriesCalculator.calculate(project, this);
+			TreeSet<CashFlowPeriod> period = (TreeSet<CashFlowPeriod>) src.getPeriodContainers().first().getPeriods();
+			StochasticChartArea stochasticChart = new StochasticChartArea(period);
+			getView().addStochasticChartArea(stochasticChart);
+		} catch (StochasticMethodException | InterruptedException e) {
+			e.printStackTrace();
+		}
 		// Für jedes Berechnungsverfahren ein Chart generieren
 
-		StochasticChartArea stochasticChart = new StochasticChartArea();
 		DeterministicChartArea deterministicChart = new DeterministicChartArea();
 
-		getView().addStochasticChartArea(stochasticChart);
 		getView().addDeterministicChartArea(deterministicChart);
 
 	}
@@ -78,6 +123,24 @@ public class OutputPresenter extends ScreenPresenter<OutputViewInterface> {
 
 	@Override
 	public void handleShowErrors(ShowErrorsOnScreenEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onComplete(StochasticResultContainer result) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProgressChange(float progress) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onError(Throwable t) {
 		// TODO Auto-generated method stub
 
 	}
--- src/main/java/dhbw/ka/mwi/businesshorizon2/ui/process/output/charts/StochasticChartArea.java
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/ui/process/output/charts/StochasticChartArea.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeSet;
 
 import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Label;
+
+import dhbw.ka.mwi.businesshorizon2.models.CashFlowPeriod;
 
 /**
  * Die StochasticChartArea komponiert die Ausgabe der Ergebnisse des
@@ -20,7 +23,7 @@ public class StochasticChartArea extends GridLayout {
 
 	private static final long serialVersionUID = 1L;
 
-	public StochasticChartArea() {
+	public StochasticChartArea(TreeSet<CashFlowPeriod> periods) {
 
 		super(3, 2);
 
@@ -29,6 +32,7 @@ public class StochasticChartArea extends GridLayout {
 
 		List<String> columns = new ArrayList<String>();
 		columns.add("Unternehmenswert");
+		columns.add("Erwartungswert");
 
 		BasicColumnChart chart = new BasicColumnChart("xAxis", columns);
 
@@ -36,7 +40,7 @@ public class StochasticChartArea extends GridLayout {
 
 		values.put("1400", new double[] { 50 });
 		values.put("1600", new double[] { 60 });
-		values.put("1800", new double[] { 80 });
+		values.put("1800", new double[] { 80, 90 });
 		values.put("2000", new double[] { 100 });
 		values.put("2200", new double[] { 80 });
 		values.put("2400", new double[] { 60 });
@@ -45,19 +49,16 @@ public class StochasticChartArea extends GridLayout {
 		chart.addValues(values);
 
 		List<String> lines = new ArrayList<String>();
-		lines.add("Verlauf der Cashflows");
+		lines.add("Erwartungswert des Cashflows");
 
 		BasicLineChart chart2 = new BasicLineChart("xAxis", lines);
 
 		Map<String, double[]> values2 = new LinkedHashMap<String, double[]>();
 
-		values2.put("2008", new double[] { 50 });
-		values2.put("2009", new double[] { 60 });
-		values2.put("2010", new double[] { 80 });
-		values2.put("2011", new double[] { 100 });
-		values2.put("2012", new double[] { 80 });
-		values2.put("2013", new double[] { 60 });
-		values2.put("2014", new double[] { 50 });
+		for (CashFlowPeriod period : periods) {
+			values2.put(Integer.toString(period.getYear()), new double[] { period.getFreeCashFlow() });
+
+		}
 
 		chart2.addValues(values2);
 
@@ -82,7 +83,7 @@ public class StochasticChartArea extends GridLayout {
 
 		this.addComponent(headline, 0, 0);
 		this.addComponent(chart, 0, 1);
-		this.addComponent(table, 1, 1);
+		this.addComponent(chart2, 1, 1);
 		this.setHeight("600px");
 		this.setWidth("2000px");
 	}
