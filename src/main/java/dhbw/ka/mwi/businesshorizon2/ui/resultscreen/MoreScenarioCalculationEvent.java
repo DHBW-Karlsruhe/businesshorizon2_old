6e783c2a2303d84aa67402fad01543f4491d06c3
Tobias Lindner <Tobias.Lindner@live.de>
2015-05-17 02:14:25 +0200

    MoreScenarioScreen bereinigt.

--- src/main/java/dhbw/ka/mwi/businesshorizon2/ui/resultscreen/MoreScenarioCalculationEvent.java
+++ src/main/java/dhbw/ka/mwi/businesshorizon2/ui/resultscreen/MoreScenarioCalculationEvent.java
@@ -1,9 +1,39 @@
+/*******************************************************************************
+ * BusinessHorizon2
+ *
+ * Copyright (C) 
+ * 2012-2013 Christian Gahlert, Florian Stier, Kai Westerholz,
+ * Timo Belz, Daniel Dengler, Katharina Huber, Christian Scherer, Julius Hacker
+ * 2013-2014 Marcel Rosenberger, Mirko Göpfrich, Annika Weis, Katharina Narlock, 
+ * Volker Meier
+ * 2014-2015 Marco Glaser, Tobias Lindner
+ *
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
 package dhbw.ka.mwi.businesshorizon2.ui.resultscreen;
 
 import com.mvplite.event.Event;
 
 import dhbw.ka.mwi.businesshorizon2.models.Project;
 
+/**
+ * Event, dass zur Anzeige des Ergebnisscreens für mehrere Szenarien geworfen wird.
+ * 
+ * @author Tobias Lindner
+ *
+ */
 public class MoreScenarioCalculationEvent extends Event {
 
 	private static final long serialVersionUID = 1L;
